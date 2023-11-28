eee7791d68fc248f668f622197f6e93a0803385a
manzi osee <85952735+manziosee@users.noreply.github.com>
2023-11-28 18:21:27 +0200

    commit #4

--- frontend/src/Components.js
+++ frontend/src/Components.js
@@ -28,4 +28,42 @@ z-index:5;
 `
 : null}
 `;
+export const SignInContainer = styled.div`
+position: absolute;
+top:0;
+height:100%;
+transition: all 0.6s ease-in-out;
+left: 0;
+width: 50%;
+z-index: 2;
+${props => (props.signinIn !== true ?`
+transform: translateX(100%);`: null)}
+`;
+export const Form = styled.form`
+background:#fffff;
+display: flex;
+align -items: center;
+flex-direction: column;
+padding: 0 50px;
+height: 100%;
+text-align: center;
+`;
+export const Title = styled.h1`
+background-color:#eee;
+border: none;
+padding: 12px;
+`;
+export const Input = styled.Input`
+background-color: #eee;
+border: none;
+padding: 12px 15px;
+margin 8px 0;
+width: 100%;
+`;
+export const Button = styled.button`
+border-radius: 20px;
+border: 1px solid #ff4b2b;
+background-color: #ff4b2b;
+color: #ffff;
+`;
 
